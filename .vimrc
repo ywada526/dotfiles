1994a6d37c93a02ce7cc1e94def485b409f4395d
ywada526 <ywada526@gmail.com>
2021-09-16 21:35:15 +0900

    Add .vimrc

---
+++ .vimrc
@@ -0,0 +1,12 @@
+syntax on
+filetype plugin indent on
+set autoindent
+set expandtab
+set tabstop=2
+set shiftwidth=2
+set backspace=2
+set number
+set hlsearch
+set wildmenu
+set wildmode=list:longest,full
+set clipboard=unnamed,unnamedplus
--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ ln -snfv $DOTFILES_PATH/.Brewfile $HOME/.Brewfile
 ln -snfv $DOTFILES_PATH/.gitconfig $HOME/.gitconfig
 ln -snfv $DOTFILES_PATH/.gitignore_global $HOME/.gitignore_global
 ln -snfv $DOTFILES_PATH/.tmux.conf $HOME/.tmux.conf
+ln -snfv $DOTFILES_PATH/.vimrc $HOME/.vimrc
 ln -snfv $DOTFILES_PATH/.zprofile $HOME/.zprofile
 ln -snfv $DOTFILES_PATH/.zshrc $HOME/.zshrc
