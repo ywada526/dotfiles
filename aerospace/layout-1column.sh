9d4c8f93502499d1b8fcb3bd50a034686f8dbc0e
ywada526 <29218439+ywada526@users.noreply.github.com>
2026-02-12 17:03:04 +0900

    Toggle window borders by keyboard shortcut

--- aerospace/layout-1column.sh
+++ aerospace/layout-1column.sh
@@ -8,9 +8,6 @@ windows="$(aerospace list-windows --workspace focused --json --format '%{app-bun
 # Need at least 1 window
 (( $(jq 'length' <<< "$windows") < 1 )) && exit 0
 
-# Disable borders
-borders width=0
-
 # Flatten into a single column (v_accordion)
 aerospace flatten-workspace-tree
 aerospace layout v_accordion || true
